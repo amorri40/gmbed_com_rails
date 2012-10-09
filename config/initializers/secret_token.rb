2012c0f89a68ed98bf196dd261eaf2bbba635517
Alasdair Morrison <amorri40@gmail.com>
2012-10-09 20:07:18 +0100

    initial commit (default rails skeleton)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GmbedCom::Application.config.secret_token = '523fda7f1553c47f8735bc8eb09b8251029c3240add9ada02104c0efe4628175237ba7d461fecfcd1f5269a26ea415c52779841244849036309d45576638294b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GmbedCom::Application.config.session_store :cookie_store, key: '_gmbed_com_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GmbedCom::Application.config.session_store :active_record_store
