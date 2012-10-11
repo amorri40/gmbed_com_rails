2029e6df9ed030f837f21fc1cefc6921db5c31e0
Alasdair Morrison <amorri40@gmail.com>
2012-10-11 19:06:30 +0100

    Switched from sqlite to mysql, homepage now displays random game, most popular games and latest games

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @games = Game.all
+    @games = Game.order("rating DESC").limit(6)
+    @random_games = Game.offset(rand(Game.count)).limit(1)
+    @recent_games = Game.order("id DESC").limit(6)
 
     respond_to do |format|
       format.html # index.html.erb
